2c5fb51409dbda802c21c2d2cff57e2ce2d83196
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2021-06-22 00:16:02 +0530

    Update c practice.c

--- c practice.c
+++ c practice.c
@@ -149,3 +149,22 @@ int main() {
 }
 OUPUT-ENTER YOUR NAME-RAUNAK
 LENGTH OF RAUNAK IS 6
+
+   "PROGRAM TO REVERSE A NUMBER"
+   #include <stdio.h>
+
+int main() {
+    int x,y=0,r;
+    printf("enter a number");
+    scanf("%d",&x);
+    while(x!=0)
+    {
+        r=x%10;
+        y=y*10+r;
+        x=x/10;
+    }
+    printf("reverse is %d",y);
+    return 0;
+}
+OUTPUT-ENTER YOUR NUMBER4753
+"3753"(ANSWER)
