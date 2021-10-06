04c6e070a2b0b0bf258a8d36af10e91fd6f57008
raunakkkk03 <85750313+raunakkkk03@users.noreply.github.com>
2021-10-06 22:08:57 +0530

    Update c practice.c

--- c practice.c
+++ c practice.c
@@ -185,3 +185,28 @@ raunak
 raunak
 raunak
 raunak
+
+#include <stdio.h>
+ main() {
+     int choice;
+     printf("enter your choice=");
+     scanf("%d",&choice);
+     switch(choice)
+     {
+         case 1:printf("sunday");
+                break;
+         case 2:printf("monday");
+                break;
+         case 3:printf("tuesday");
+                break;
+         case 4:printf("wednesday");
+                break;
+         case 5:printf("thursday");
+                break;
+         case 6:printf("friday");
+                break;
+         case 7:printf("saturday");
+                break;
+     }
+     OUTPUT-enter your choice=4
+wednesday
